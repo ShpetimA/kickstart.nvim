351f25b6af54299b3b8398c1bd45811bab2a617f
Shpetim <shpetim.alimi@ndbit.net>
2024-06-09 16:16:17 +0200

    Removed unnescescary telescope keybindings

--- init.lua
+++ init.lua
@@ -29,7 +29,7 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
